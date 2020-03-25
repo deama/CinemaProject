721a487151b307146e80aee3cf4970215625f68b
deama <deama@mail.com>
2020-03-25 12:48:36 +0000

    added payment page. Created payment and booking controllers by removing the corresponding functions from HomeController. Fixed bug in booking.scala.html where wouldn't forward movieTitle.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,23 +2,14 @@ package controllers
 
 import authentication.AuthenticationAction
 import javax.inject.{Inject, Singleton}
-import models.BookingForm
+import models.{BookingForm, PaymentForm}
 import play.api.mvc.{AbstractController, Action, AnyContent, Call, ControllerComponents, Request}
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, authAction: AuthenticationAction, val app: DBManager) extends AbstractController(cc) with play.api.i18n.I18nSupport
 {
-  def index() :Action[AnyContent] = authAction
-  {
-    Ok( views.html.index("") )
-  }
-
-  def bookingSubmit( movieTitle :String ) :Action[AnyContent] = authAction { implicit request :Request[AnyContent] =>
-    BookingForm.bookingForm.bindFromRequest.fold({ formWithErrors =>
-      BadRequest( views.html.booking(formWithErrors, movieTitle) )
-    }, { bookingForm =>
-      Redirect( routes.DBManager.createBooking(movieTitle, bookingForm.screeningTime,
-        bookingForm.nameOfBooker, bookingForm.adults, bookingForm.children, bookingForm.concession) )
-    })
+  def index() :Action[AnyContent] = authAction { implicit request :Request[AnyContent] =>
+    //Ok( views.html.index("") )
+    Ok( views.html.booking(BookingForm.bookingForm, "asdf") )
   }
 }
---
+++ app/models/PaymentData.scala
@@ -0,0 +1,19 @@
+package models
+
+import java.util.Date
+
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.libs.json.OFormat
+import reactivemongo.bson.BSONObjectID
+
+case class PaymentData(id :String, name :String, cardNumber :String, expDate :String, securityCode :String )
+
+object PaymentData
+{
+  import reactivemongo.play.json._
+  import reactivemongo.play.json.collection.JSONCollection
+  import play.api.libs.json._
+
+  implicit val bookingFormat :OFormat[PaymentData] = Json.format[PaymentData]
+}
