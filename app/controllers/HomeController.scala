d144a6b2545bf4a042023c5643919390c539b8fb
deama <deama@mail.com>
2020-03-30 16:26:32 +0000

    Made navigation consistant through pages.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -9,6 +9,7 @@ import play.api.mvc.{AbstractController, Action, AnyContent, Call, ControllerCom
 class HomeController @Inject()(cc: ControllerComponents, authAction: AuthenticationAction, val app: DBManager) extends AbstractController(cc) with play.api.i18n.I18nSupport
 {
   def index() :Action[AnyContent] = authAction { implicit request :Request[AnyContent] =>
+    //Ok( views.html.index("QACinema") )
     Ok( views.html.home() )
     //Ok( views.html.booking(BookingForm.bookingForm, "asdf") )
   }
