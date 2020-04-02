2bf471aafe4db9c820cec5637fb48b98d270bd7e
deama <deama@mail.com>
2020-04-02 13:18:01 +0100

    Fixed places to go.

--- app/controllers/placestogoController.scala
+++ app/controllers/placestogoController.scala
@@ -5,7 +5,7 @@ import javax.inject._
 import play.api.mvc._
 
 @Singleton
-class PlacesToGoController @Inject()(cc: ControllerComponents, authAction: AuthenticationAction) extends AbstractController(cc) {
+class placestogoController @Inject()(cc: ControllerComponents, authAction: AuthenticationAction) extends AbstractController(cc) {
   def places(): Action[AnyContent] = Action {
     Ok(views.html.placestogo())
   }
